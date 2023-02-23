db857a17ca527cdc1a026e0b9dbf78607a88dc83
verrannt <pascal.schroeder@ru.nl>
2023-02-23 13:35:39 +0100

    update testing class conditional loader

--- tests/test_dataloaders.py
+++ tests/test_dataloaders.py
@@ -75,15 +75,30 @@ def test_variants_class_conditional():
 
 def test_class_conditional_loader():
     conditional_loader = ClassConditionalLoader(words_file='/home/passch/data/HP_VariaNTS_intersection.txt')
-    print(conditional_loader('path/to/good/things/bed_timestretch1.wav'))
-    print(conditional_loader('bed'))
-    print(conditional_loader('boel'))
-    print(conditional_loader('brief'))
-    print(conditional_loader('half'))
-    print(conditional_loader('meer'))
-    print(conditional_loader('zet'))
-    print(conditional_loader('zin'))
-    print(conditional_loader('zoon'))
+
+    import torch
+    import time
+    start = time.time()
+    out1 = torch.stack([conditional_loader('path/to/good/things/bed_timestretch1.wav') for _ in range(12)], dim=0)
+    end = time.time()
+    print(out1.shape)
+    print(end-start)
+    
+    start = time.time()
+    out2 = conditional_loader.batch_call(['path/to/good/things/bed_timestretch1.wav']*12)
+    end = time.time()
+    print(out2.shape)
+    print(end-start)
+
+    # print(conditional_loader('path/to/good/things/bed_timestretch1.wav'))
+    # print(conditional_loader('bed'))
+    # print(conditional_loader('boel'))
+    # print(conditional_loader('brief'))
+    # print(conditional_loader('half'))
+    # print(conditional_loader('meer'))
+    # print(conditional_loader('zet'))
+    # print(conditional_loader('zin'))
+    # print(conditional_loader('zoon'))
 
 
 if __name__=='__main__':
