119dc7ad92d616682866e821eca3c608b1005282
verrannt <pascal.schroeder@ru.nl>
2023-03-17 09:23:33 +0100

    adapt brain encoder to subject 2 brain data

--- models/brain_encoder.py
+++ models/brain_encoder.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 """
 Input:
-[B, E, F, T] (batch size, electrodes (channels), frequency bands, timesteps)
+[B, E, T] (batch size, electrodes (channels), timesteps)
 
 Output:
 [B, C, L=16000]
@@ -16,30 +16,35 @@ class BrainEncoder(nn.Module):
         c_in : int = 32,
         c_mid : int = 64,
         c_out : int = 128,
-        stride_y : int = 12,
-        pad_y : int = 8,
         **kwargs,
     ):
         super().__init__()
 
-        _kernel_size = 5
+        _kernel_size = 18
+        _padding = 3
+        _stride = 6
 
-        conv1 = nn.ConvTranspose2d(c_in, c_mid, (1, _kernel_size), padding=(0, pad_y), stride=(1, stride_y))
+        conv1 = nn.ConvTranspose1d(c_in, c_mid, _kernel_size, padding=_padding, stride=_stride + 2)
         conv1 = nn.utils.weight_norm(conv1)
         nn.init.kaiming_normal_(conv1.weight)
 
-        conv2 = nn.ConvTranspose2d(c_mid, c_out, (2, _kernel_size*2), padding=(1, pad_y+2), stride=(1, stride_y+2))
+        conv2 = nn.ConvTranspose1d(c_mid, c_mid, _kernel_size, padding=_padding, stride=_stride)
         conv2 = nn.utils.weight_norm(conv2)
         nn.init.kaiming_normal_(conv2.weight)
+        
+        conv3 = nn.ConvTranspose1d(c_mid, c_out, _kernel_size, padding=_padding, stride=_stride)
+        conv3 = nn.utils.weight_norm(conv3)
+        nn.init.kaiming_normal_(conv3.weight)
 
         self.net = nn.Sequential(
             conv1,
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             conv2,
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
+            conv3,
+            nn.ReLU(),
         )
 
     def forward(self, x):
         x = self.net(x)
-        x = torch.squeeze(x, 2) # (B, C, 1, L) -> (B, C, L)
         return x
